bbe2c163e96a5b313639733851175b218d4c5c76
David Larsson <37840889+davidkwlarsson@users.noreply.github.com>
2020-02-27 16:03:13 +0100

    Add files via upload

--- Modified_EfficientDet/help_functions.py
+++ Modified_EfficientDet/help_functions.py
@@ -192,19 +192,19 @@ def dataGenerator(dir_path, batch_size = 16, data_set = 'training'):
     if data_set == 'training':
         xyz_list = json_load(os.path.join(dir_path, 'training_xyz.json'))[:-560]
         xyz_list *= 4
-        num_samples = len(xyz_list) # - 300 # MINUS THE SAMPLES IN VALIDATION SET
-        print("Total number of training samples: ", num_samples)
+        num_samples = len(xyz_list)
         K_list = json_load(os.path.join(dir_path, 'training_K.json'))[:-560]
         K_list *= 4
-        indicies = [i for i in range(31999)] + [i for i in range(32560,64563)] + [i for i in range(65120,97119)] + [i for i in range(97680,129679)]
+        indicies = [i for i in range(32000)] + [i for i in range(32560,64560)] + [i for i in range(65120,97120)] + [i for i in range(97680,129680)]
+        print("Total number of training samples: ", num_samples, " and ", len(indicies))
     elif data_set == 'validation':
         xyz_list = json_load(os.path.join(dir_path, 'training_xyz.json'))[-560:]
         xyz_list *= 4
         num_samples = len(xyz_list)
-        print("Total number of validation samples: ", num_samples)
         K_list = json_load(os.path.join(dir_path, 'training_K.json'))[-560:]
         K_list *= 4
-        indicies = [i for i in range(31999,32560)] + [i for i in range(64563,65120)] + [i for i in range(97119, 97680)] + [i for i in range(129679,130240)]
+        indicies = [i for i in range(32000,32560)] + [i for i in range(64560,65120)] + [i for i in range(97120, 97680)] + [i for i in range(129680,130240)]
+        print("Total number of validation samples: ", num_samples," and ", len(indicies))
     elif data_set == 'evaluation':
         xyz_list = json_load(os.path.join(dir_path, 'evaluation_xyz.json'))
         num_samples = len(xyz_list)
